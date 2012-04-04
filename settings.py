7c01cbe76526aba3dddb55046d431c43340b1564
Ben Glynn <benglynn@benglynn.net>
2012-04-04 17:31:25 +0100

    Updated to match the 1.3.1 settings, also tidied up a little.

--- settings.py
+++ settings.py
@@ -1,48 +1,23 @@
 import os
 import sys
 
+PRODUCTION =\
+    os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine')\
+    or os.getenv('SETTINGS_MODE') == 'prod'
+
 HTTP_HOST = os.environ.get('HTTP_HOST')
 
 PROJDIR = os.path.abspath(os.path.dirname(__file__))
 APPDIR = os.path.join(PROJDIR, 'core')
 
-# todo: duplication of that in main.py?
-sys.path.extend([os.path.join(PROJDIR, 'lib')])
-
-# Set by main.py to '1' on dev and '0' on production
-DJANGO_DEBUG = os.environ.get('DJANGO_DEBUG', '1')
-DEBUG = bool(int(DJANGO_DEBUG))
+DEBUG = not PRODUCTION
 TEMPLATE_DEBUG = DEBUG
 
-# Uncomment these DB definitions to use Cloud SQL.  MySQL is used locally for
-# development. This is not a requirement, you can also connect to Cloud SQL in
-# your development environment, whether that be the production instance or a
-# development instance.
-# See https://developers.google.com/cloud-sql/docs/django#development-settings
-"""
-# CLoud SQL with local mysql substitue for dev
-if (os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine') or
-    os.getenv('SETTINGS_MODE') == 'prod'):
-    # Running on production App Engine, so use a Google Cloud SQL database.
-    DATABASES = {
-        'default': {
-            'ENGINE': 'google.appengine.ext.django.backends.rdbms',
-            'INSTANCE': 'my_project:instance1',
-            'NAME': 'my_db',
-        }
-    }
-else:
-    # Running in development, so use a local MySQL database.
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'USER': '',
-            'PASSWORD': '',
-            'HOST': 'localhost',
-            'NAME': '',
-        }
-    }
-"""
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
 
 # A custom cache backend using AppEngine's memcached
 CACHES = {
@@ -51,54 +26,109 @@ CACHES = {
         }
 }
 
-# Custom session engine using our cache or writing through to the datastore
-# If using SQL, can we use django's standard write through?
-# If gae memecached is stable enough, it would be faster to use:
-# django.contrib.sessions.backends.cache
+"""
+Custom session engine using our cache or writing through to the datastore If
+using SQL, can we use django's standard write through?  If gae memecached is
+stable enough, it would be faster to use
+django.contrib.sessions.backends.cache?
+"""
+
 SESSION_ENGINE = "appengine_sessions.backends.cached_db"
 
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
+# Uncomment these DB definitions to use Cloud SQL.
+# See: https://developers.google.com/cloud-sql/docs/django#development-settings
 
-MANAGERS = ADMINS
+"""
+import os
+if (os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine') or
+    os.getenv('SETTINGS_MODE') == 'prod'):
+    # Running on production App Engine, so use a Google Cloud SQL database.
+    DATABASES = {
+        'default': {
+            'ENGINE': 'google.appengine.ext.django.backends.rdbms',
+            'INSTANCE': 'my_project:instance1',
+            'NAME': 'my_db',
+            }
+        }
+else:
+    # Running in development, so use a local MySQL database.
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'USER': 'user',
+            'PASSWORD': 'pass',
+            'HOST': 'localhost',
+            'NAME': 'my_db',
+            }
+        }
+"""
 
+
+
+# Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
 TIME_ZONE = 'America/Chicago'
 
+# Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-USE_I18N = False
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
 
-USE_L10N = False
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
 
-template_context_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.contrib.messages.context_processors.messages',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.contrib.messages.context_processors.messages',)
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/static/'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = ''
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = ''
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '!cjx1x7vn8ukbemz#_tr99nzethtpsn%f_n-737)$#-#goj(x-'
+SECRET_KEY = '^(&s11q!@t2j@=dgpp65k+df6o1(@1h9cq-$^p@=k4!5))xi6u'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -110,8 +140,8 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
@@ -121,16 +151,41 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(APPDIR, 'templates')
 )
 
 INSTALLED_APPS = (
-    'appengine_sessions',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'core'
+    'django.contrib.staticfiles',
+    'core',
+    # Uncomment the next line to enable the admin:
+    # 'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
 )
 
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
