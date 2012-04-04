b1cf95e5f4d0850b773364b5ffdb32e11437b03a
Ben Glynn <benglynn@benglynn.net>
2012-04-04 17:29:17 +0100

    Tidied up and refactored a little.

--- main.py
+++ main.py
@@ -1,44 +1,30 @@
 import os
 import sys
-import django
 import logging
-
-"""
-for key, value in os.environ.iteritems():
-    logging.info('%s: %s' % (key, value))
-"""
-
-sys.path.extend(['lib'])
-
-dev = os.environ['SERVER_NAME'] == 'localhost'
-
-os.environ['DJANGO_DEBUG'] = dev and '1' or '0'
-
-if dev:
-    logging.info('Development django: %s' % django.__file__)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-import django.core.handlers.wsgi
-app = django.core.handlers.wsgi.WSGIHandler()
-
-
-'''
-    Logging configuration
-        with Django 1.2 need to set up a log_exception method so
-        the error appears within the Logs section on app engine admin
-        
-        Connect log_exception as a Signal
-        see : http://code.google.com/appengine/articles/django.html
-
-'''
-
-def log_exception(*args, **kwds):
-    logging.exception('Exception in request:')
-
+import django
 import django.core.signals
 import django.dispatch
 import django.db
+
+sys.path.extend(['lib'])
+
+PRODUCTION =\
+    os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine') or\
+    os.getenv('SETTINGS_MODE') == 'prod'
+
+if not PRODUCTION:
+    logging.info('Development django: %s' % django.__file__)
+    logging.info(django.get_version())
+
+
+# Logging configuration
+# see: http://code.google.com/appengine/articles/django.html
+
+def log_exception(*args, **kwds):
+    logging.exception('Exception in request:')
 
 # Log errors.
 django.dispatch.Signal.connect(
@@ -49,3 +35,6 @@ django.dispatch.Signal.disconnect(
     django.core.signals.got_request_exception,
     django.db._rollback_on_exception)
 
+# WSGI app
+import django.core.handlers.wsgi
+app = django.core.handlers.wsgi.WSGIHandler()
